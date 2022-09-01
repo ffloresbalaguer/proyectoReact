73a035aa72507f0e0d0673e96a0f7e8a8f9f9dd0
Francisco Flores Balaguer <ffloresbalaguer@hotmail.com>
2022-08-31 23:24:12 -0300

    Map1.5

--- my-app/src/components/Item/Item.jsx
+++ my-app/src/components/Item/Item.jsx
@@ -53,22 +53,8 @@ const Item = () => {
   )
 }
 
-   /* consultarPromesa(true)
-    .then(producto => {
-        setItem(producto)
-    })
-    .catch(error => {
-        console.error(error)
-    })
 
-    console.log(productos) */
 
 export default Item;
 
-/*<div className="card border-primary mb-3" style={{maxWidth: '20rem'}}>
-  <div className="card-header">Header</div>
-  <div className="card-body">
-    <h4 className="card-title">Primary card title</h4>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>*/
+
