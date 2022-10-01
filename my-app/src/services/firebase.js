c49cf1b08e122adb3da14192f76a51908c74034d
Francisco Flores Balaguer <ffloresbalaguer@hotmail.com>
2022-10-01 18:17:00 -0300

    APIKEY

--- my-app/src/services/firebase.js
+++ my-app/src/services/firebase.js
@@ -8,7 +8,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
+  apiKey: process.env.REACT_APP_apiKey,
   authDomain: "proyecto-react-ffb.firebaseapp.com",
   projectId: "proyecto-react-ffb",
   storageBucket: "proyecto-react-ffb.appspot.com",
